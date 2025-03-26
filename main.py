5219f3a3b82874db6024d593d6a42b6427f2ce29
Ubuntu <ubuntu@ip-10-0-12-250.ap-southeast-2.compute.internal>
2025-03-26 06:18:23 +0000

    fix main

--- main.py
+++ main.py
@@ -1,12 +1,14 @@
 from dem_comparison.analysis import analyse_difference
 import argparse
+from pathlib import Path
 
 parser = argparse.ArgumentParser()
 parser.add_argument("--lat-range", type=range, default=range(-90, -50))
 parser.add_argument("--lon-range", type=range, default=range(-180, 180))
 parser.add_argument("--rema-resolution", type=int, default=10)
 parser.add_argument("--interval", type=int, default=10)
-parser.add_argument("--use_multiprocessing", action="store_false")
+parser.add_argument("--use-multiprocessing", action="store_false")
+parser.add_argument("--save-dir-path", type=str, default="Antarctica_Dem_Comparison")
 
 args = parser.parse_args()
 
@@ -18,4 +20,5 @@ if __name__ == "__main__":
         rema_resolution=args.rema_resolution,
         interval=args.interval,
         use_multiprocessing=args.use_multiprocessing,
+        save_dir_path=Path(args.save_dir_path),
     )
