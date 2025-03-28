b5a35417b82ee9ea2eb88a9d051fc674edbd8f4a
Ubuntu <ubuntu@ip-10-0-12-250.ap-southeast-2.compute.internal>
2025-03-28 01:38:10 +0000

    improve plots

--- dem_comparison/plots.py
+++ dem_comparison/plots.py
@@ -1,5 +1,5 @@
 import plotly.graph_objects as go
-import pickle
+from plotly.subplots import make_subplots
 from pathlib import Path
 from dem_comparison.utils import read_metrics
 
@@ -9,6 +9,7 @@ def plot_metrics(
     is_error: bool = True,
     polar: bool = False,
     save_path: Path | None = None,
+    data_bounds: tuple | list[tuple] | None = (-50, 50),
 ) -> go.Figure:
     """Plots the metrics for the DEMS as an interactive plot
 
@@ -22,36 +23,63 @@ def plot_metrics(
         Polar plot, by default False
     save_path: Path | None, optional,
         Path to save the html plot, by default None
-
+    data_bounds: tuple | list[tuple] | None, optional
+        Filters out the data beyond the given bounds (left and right inclusive), by default (-50, 50),
+        If list of bounds provided the number of bounds should be the same as the number of metrics.
     Returns
     -------
     go.Figure
         Plotly figure
     """
 
+    def filter_data(met, db):
+        validity_list = [True if db[0] <= el <= db[1] else False for el in met]
+        new_metric = [el for j, el in enumerate(met) if validity_list[j]]
+        new_x = [el for j, el in enumerate(x) if validity_list[j]]
+        new_y = [el for j, el in enumerate(y) if validity_list[j]]
+        return new_x, new_y, new_metric
+
     labels = ["ME" if is_error else "MEAN", "STD", "MSE", "NMAD"]
     metrics, x, y = read_metrics(metric_files, numerical_axes=polar)
+
+    if type(data_bounds) is list:
+        assert len(data_bounds) == len(
+            metrics
+        ), "If used as a list, the number of data bounds should be the same as the number of metrics."
+
     # buttons to create
     buttons = [
         {
             "method": "restyle",
             "label": labels[i],
             "args": [
-                {"visible": [l == labels[i] for l in labels]},
                 {
-                    "z": [metric],
+                    "visible": [
+                        el for el in [l == labels[i] for l in labels] for _ in range(2)
+                    ]
                 },
             ],
         }
-        for i, metric in enumerate(metrics)
+        for i in range(len(metrics))
     ]
 
     # Add dropdown
     updatemenu = [{"buttons": buttons, "direction": "down", "showactive": True}]
 
-    fig = go.Figure()
-    fig.update_layout(xaxis_title="Lat", yaxis_title="Lon")
+    fig = make_subplots(
+        rows=1,
+        cols=2,
+        specs=[[{"type": "polar" if polar else "xy"}, {"type": "xy"}]],
+        column_widths=[0.7, 0.3],
+    )
+    if not polar:
+        fig.update_layout(xaxis_title="Lat", yaxis_title="Lon")
     for i, metric in enumerate(metrics):
+        if data_bounds is not None:
+            if type(data_bounds) is list:
+                x, y, metric = filter_data(metric, data_bounds[i])
+            else:
+                x, y, metric = filter_data(metric, data_bounds)
         if polar:
             fig.add_trace(
                 go.Scatterpolar(
@@ -60,9 +88,15 @@ def plot_metrics(
                     mode="markers",
                     marker=dict(color=metric, colorbar=dict(thickness=20)),
                     name="",
-                    hovertext=metric,
+                    hovertemplate="<i>Lat</i>: %{theta}, "
+                    + "<i>Lon</i>: %{r}"
+                    + "<br></br>"
+                    "<b>%{text}</b>",
+                    text=[f"{labels[i]}: {j}" for j in metric],
                     visible=True if i == 0 else False,
-                )
+                ),
+                row=1,
+                col=1,
             )
         else:
             fig.add_trace(
@@ -74,8 +108,20 @@ def plot_metrics(
                     name="",
                     hovertext=metric,
                     visible=True if i == 0 else False,
-                )
+                ),
+                row=1,
+                col=1,
             )
+
+        fig.add_trace(
+            go.Histogram(
+                x=metric,
+                visible=True if i == 0 else False,
+                showlegend=False,
+            ),
+            row=1,
+            col=2,
+        )
 
     fig.update_layout(updatemenus=updatemenu)
     if polar:
